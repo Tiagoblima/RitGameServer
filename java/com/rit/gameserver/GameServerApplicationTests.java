a32758a41747fdd1c7aeb0350e953c1d8429a5a3
Tiago de Lima <tiago.blima02@gmail.com>
2020-04-30 11:55:22 -0300

    Testing controller request /getAll

--- java/com/rit/gameserver/GameServerApplicationTests.java
+++ java/com/rit/gameserver/GameServerApplicationTests.java
@@ -4,17 +4,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.rit.gameserver.api.GameController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest()
+@AutoConfigureMockMvc
 class GameServerApplicationTests {
+
+	private static final String base_url = "/api/v1/game";
 
 	@Autowired
 	private GameController controller;
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
 	void contextLoads() {
 		assertThat(controller).isNotNull();
+	}
+
+	@Test
+	public void testGameList() throws Exception{
+		this.mockMvc.perform(get(base_url+"/getAll")).andDo(print()).andExpect(status().isOk());
 	}
 
 }
