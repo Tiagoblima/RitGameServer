000bae5188176eeedf69bce2f07f3092fc439627
Tiago de Lima <tiago.blima02@gmail.com>
2020-04-30 10:44:45 -0300

    Making the first sanity test

--- java/com/rit/gameserver/GameServerApplicationTests.java
+++ java/com/rit/gameserver/GameServerApplicationTests.java
@@ -1,13 +1,20 @@
 package com.rit.gameserver;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import com.rit.gameserver.api.GameController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GameServerApplicationTests {
 
+	@Autowired
+	private GameController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
